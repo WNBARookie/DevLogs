190023c6ea74a60458f55f27795cb1695eaeaf69
Trinity <trinity@Trinitys-MacBook-Air.local>
2025-02-22 10:34:02 -0600

    added routing, primeng, and basic home page

--- dev-logs-ui/src/app/app.routes.ts
+++ dev-logs-ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Default route redirects to /home
+  { path: 'home', component: HomeComponent }, // Route for HomeComponent
+  { path: '**', redirectTo: 'home' }, // Wildcard route redirects to /home
+];
---
+++ dev-logs-ui/src/app/features/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ButtonModule, CardModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
