f48b0bf2ffd151be38722f14b249f3681b9f0012
Trinity <trinity@Trinitys-MacBook-Air.local>
2025-02-24 08:33:46 -0600

    cleaned up getting all categories by implementing error handling

--- dev-logs-api/src/controllers/categoryController.ts
+++ dev-logs-api/src/controllers/categoryController.ts
@@ -1,11 +1,14 @@
 import expressAsyncHandler from 'express-async-handler';
 import { CategoryModel } from '../models';
+import { ApiResponse } from '../utils';
 
 // @access  Private
 // @desc    Get all categories
 // @route   GET /api/categories/all
-export const getAllCategories = expressAsyncHandler(async (req: any, res) => {
-  const categories = await CategoryModel.find();
+export const getAllCategories = expressAsyncHandler(async (req: any, res, next) => {
+  const categories = await CategoryModel.find()
+    .then((data) => data)
+    .catch((err) => next(ApiResponse.badRequest('Error', err, 400, req.originalUrl)));
 
   res.status(200).json(categories);
 });
