f119b4300a75c91d0a204a18b9822de0be948c3a
Trinity <trinity@Trinitys-MacBook-Air.local>
2025-09-22 19:47:40 -0500

    add host for railway

--- dev-logs-ui/vite.config.ts
+++ dev-logs-ui/vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 4200,
-    allowedHosts: ['devlogs-ui-production.up.railway.app'],
+    allowedHosts: ['devlogs-ui-production.up.railway.app', 'www.dev-logs.dev'],
   },
 });
